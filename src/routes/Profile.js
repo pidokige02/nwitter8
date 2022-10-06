1020978f454fd56f2d5eed0fadc779c3e4063eeb
pidokige02 <pidokige0204@gmail.com>
2022-10-06 22:55:48 +0900

    #2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,18 @@
 import React from "react";
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+
+export default Profile;
