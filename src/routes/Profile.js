1e59ca53419458afa8aa4182744f929f10bc3ee3
pidokige02 <pidokige0204@gmail.com>
2022-10-09 06:53:51 +0900

    #5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,29 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
-  
-  const getMyNweets = async () => {
-    const nweets = await dbService
-      .collection("nweets")
-      .where("creatorId", "==", userObj.uid)
-      .orderBy("createdAt")
-      .get();
-    console.log(nweets.docs.map((doc) => doc.data()));
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
   };
 
-  useEffect(() => {
-    getMyNweets();
-  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      // displayName 과 photoURL 만 firebase 에서는 지원이 가능하다.
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+
+  // const getMyNweets = async () => {
+  //   const nweets = await dbService
+  //     .collection("nweets")
+  //     .where("creatorId", "==", userObj.uid)
+  //     .orderBy("createdAt")
+  //     .get();
+  //   console.log(nweets.docs.map((doc) => doc.data()));
+  // };
+
+  // useEffect(() => {
+  //   getMyNweets();
+  // }, []);
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
